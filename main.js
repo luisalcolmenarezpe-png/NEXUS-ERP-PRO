e8b0a7d5dade7c636099f3c220024f0a275ab9a7
luisalcolmenarezpe-png <luisalcolmenarezpe@gmail.com>
2026-08-14 15:01:43 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    title: "Nexus ERP Pro"
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
